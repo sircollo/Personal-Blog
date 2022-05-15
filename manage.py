9eb97a6b3052af3b66f63d3f83dac39027dbef03
Collins Sirwani <sircollo71@gmail.com>
2022-05-15 14:38:44 +0300

    This will add flask admin command to allow admin access and adds migrate command

--- manage.py
+++ manage.py
@@ -1,12 +1,22 @@
 from app import create_app,db
 from flask_script import Manager,Server
+from app.models import User,Blog,Comment
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+from flask_migrate import Migrate,MigrateCommand
 
 app = create_app('development')
 
 manager = Manager(app)
 manager.add_command('server',Server)
 
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 
+admin = Admin(app)
+admin.add_view(ModelView(User,db.session))
+admin.add_view(ModelView(Blog,db.session))
+admin.add_view(ModelView(Comment,db.session))
 
 @manager.shell
 def make_shell():
