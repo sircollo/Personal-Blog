bf1fd6ae55ff7d290d21be85bb34e1827bc0605b
Collins Sirwani <sircollo71@gmail.com>
2022-05-16 09:14:44 +0300

    This will add method funtions to blogs and also adds subscriber class model

--- app/models.py
+++ app/models.py
@@ -27,19 +27,26 @@ class User(db.Model,UserMixin):
 class Blog(db.Model,UserMixin):
   __tablename__='blogs'
   id = db.Column(db.Integer,primary_key = True)
-  title = db.Column(db.String(255))
+  title = db.Column(db.String(30))
+  author = db.Column(db.String(25))
   blog_post = db.Column(db.String(255))
   date_posted = db.Column(db.DateTime,default=datetime.utcnow)
   comments = db.relationship('Comment',backref='blog',lazy='dynamic')
+
   
   # poster = db.Column(db.LargeBinary)
   
   def save_blog(self):
     db.session.add(self)
+    db.session.commit()
+    
+  def delete(self):
+    db.session.delete(self)
+    db.session.commit()
     
   @classmethod
-  def get_blog(cls, blog_id):
-    blogs = Blog.query.filter_by(blog_id=blog_id).all()
+  def get_blog(cls, id):
+    blogs = Blog.query.filter_by(id=id).all()
     return blogs
   
 
@@ -48,12 +55,16 @@ class Comment(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   comment = db.Column(db.String(255))
   blog_id = db.Column(db.Integer,db.ForeignKey('blogs.id',ondelete='SET NULL'),nullable = True) 
-  
+  blog_pic = db.Column(db.String(255))
   
   def save_comment(self):
     if self not in db.session:
         db.session.add(self)
         db.session.commit()
+  def delete(self):
+      db.session.remove(self)
+      db.session.commit()
+
         
   @classmethod
   def get_comments(cls,blog_post):
@@ -62,6 +73,19 @@ class Comment(db.Model):
    
   def __repr__(self):
     return f'Comment {self.comment}'
+  
+class Subscriber(db.Model):
+    __tablename__='subscribers'
+
+    id=db.Column(db.Integer,primary_key=True)
+    email = db.Column(db.String(255),unique=True,index=True)
+
+    def save_subscriber(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f'Subscriber {self.email}'
     
     
   
