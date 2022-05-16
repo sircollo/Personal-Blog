9c619c62920758f697273d9e6bde3da1ed4b039d
Collins Sirwani <sircollo71@gmail.com>
2022-05-16 17:44:17 +0300

    This will add username field

--- app/main/forms.py
+++ app/main/forms.py
@@ -6,3 +6,8 @@ class CommentsForm(FlaskForm):
     comment = TextAreaField('Add a comment',validators = [DataRequired()])
     
     submit = SubmitField('Submit')
+    
+class SubscribeForm(FlaskForm):
+    username = StringField('Name',validators = [DataRequired()])
+    email = StringField('Email',validators = [DataRequired()])    
+    submit = SubmitField('Subscribe')
