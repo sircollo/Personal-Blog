f835b9bc495e641d822e368c57306ea806d6a773
Collins Sirwani <sircollo71@gmail.com>
2022-05-16 14:15:17 +0300

    This will add a comments flaskform

---
+++ app/main/forms.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,SelectField,SubmitField,TextAreaField
+from wtforms.validators import DataRequired
+
+class CommentsForm(FlaskForm):
+    comment = TextAreaField('Add a comment',validators = [DataRequired()])
+    
+    submit = SubmitField('Submit')
