fb65aaea5076c72c7a4c1cc2abce364a31ebf356
Collins Sirwani <sircollo71@gmail.com>
2022-05-16 14:33:29 +0300

    This will add email configuration files

--- config.py
+++ config.py
@@ -2,6 +2,14 @@ import os
 
 class Config:
   SQLALCHEMY_TRACK_MODIFICATIONS = False
+  SECRET_KEY = os.environ.get('SECRET_KEY')
+#  email configurations
+  MAIL_SERVER = 'smtp.googlemail.com'
+  MAIL_PORT = 587
+  MAIL_USE_TLS = True
+  MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+  MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+  
 
 class ProdConfig(Config):
   pass
